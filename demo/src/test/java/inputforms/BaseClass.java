5ceb1825fad105ad3f2a0b4dd0d279e67f31a445
Subhani Shaik <Subhani.Shaik@dentsuaegis.com>
2018-07-14 17:24:47 +0100

    Added firefox browser

--- demo/src/test/java/inputforms/BaseClass.java
+++ demo/src/test/java/inputforms/BaseClass.java
@@ -3,16 +3,21 @@ package inputforms;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
 
     public WebDriver openSeleniumEasySite() throws InterruptedException {
-       // System.setProperty("webdriver.chrome.driver", "C://Users//subhani//Downloads//chromedriver.exe");
 
-        System.setProperty("webdriver.chrome.driver", "/Users/sshaik02/Downloads/chromedriver");
+        //Chrome browser
+        // System.setProperty("webdriver.chrome.driver", "C://Users//subhani//Downloads//chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "/Users/sshaik02/Downloads/chromedriver");
+//        WebDriver driver = new ChromeDriver();
 
+        //Firefox browser
+        System.setProperty("webdriver.gecko.driver","/Users/sshaik02/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
 
-        WebDriver driver = new ChromeDriver();
 
         driver.get("http://www.seleniumeasy.com/test/basic-first-form-demo.html");
 
