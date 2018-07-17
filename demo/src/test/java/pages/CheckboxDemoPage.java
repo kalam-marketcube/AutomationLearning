b2b8c3d7247bd2d743e8775dd3e608d3fd6158c2
Subhani Shaik <Subhani.Shaik@dentsuaegis.com>
2018-07-17 17:32:31 +0100

    Created Checkbox Demo page object class

---
+++ demo/src/test/java/pages/CheckboxDemoPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CheckboxDemoPage {
+
+    WebDriver driver;
+
+    public CheckboxDemoPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
